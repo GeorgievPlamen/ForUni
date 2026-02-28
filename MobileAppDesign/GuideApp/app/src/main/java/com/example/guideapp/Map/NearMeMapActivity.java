6815bdd6a2b59d7c6f599d5d16730267f82d11fa
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2026-02-28 13:10:12 +0200

    Finish

--- MobileAppDesign/GuideApp/app/src/main/java/com/example/guideapp/Map/NearMeMapActivity.java
+++ MobileAppDesign/GuideApp/app/src/main/java/com/example/guideapp/Map/NearMeMapActivity.java
@@ -1,5 +1,6 @@
 package com.example.guideapp.Map;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.guideapp.Details.DetailsActivity;
 import com.example.guideapp.R;
 
 public class NearMeMapActivity extends AppCompatActivity {
@@ -17,6 +19,7 @@ public class NearMeMapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_near_me_map);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.map_root), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,5 +27,11 @@ public class NearMeMapActivity extends AppCompatActivity {
         });
 
         findViewById(R.id.btn_back).setOnClickListener(v -> finish());
+
+        // View details -> DetailsActivity
+        findViewById(R.id.btn_view_details).setOnClickListener(v -> {
+            Intent intent = new Intent(NearMeMapActivity.this, DetailsActivity.class);
+            startActivity(intent);
+        });
     }
 }

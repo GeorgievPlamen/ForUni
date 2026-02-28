809ea545ca655ebdb16d9cc72b1a8fcd10a5dda5
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2026-02-28 12:41:13 +0200

    Location Details

---
+++ MobileAppDesign/GuideApp/app/src/main/java/com/example/guideapp/Details/DetailsActivity.java
@@ -0,0 +1,29 @@
+package com.example.guideapp.Details;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.guideapp.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_details);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.details_root), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        findViewById(R.id.btn_back_details).setOnClickListener(v -> finish());
+    }
+}
